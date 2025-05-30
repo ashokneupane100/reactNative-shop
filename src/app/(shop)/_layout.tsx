ee9bb6e9d6376823d8a487fa7dcaaefc26b0615e
Ashok Neupane <ashokabrother@gmail.com>
2025-05-30 11:58:25 +0545

    tabs and stack screens done nicely  #35.52

---
+++ src/app/(shop)/_layout.tsx
@@ -0,0 +1,68 @@
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome name={props.name} size={24} color={props.color} />;   
+}
+
+const TabsLayout = () => {
+  return (
+    <SafeAreaView edges={["top"]} style={styles.safeArea}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#1BC464",
+          tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontWeight: "bold",
+          },
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 8,
+            paddingBottom: 18,
+            height: 100,
+            position: 'absolute',
+            bottom: 10,
+            left: 0,
+            right: 0,
+          },
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Shop",
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name="shopping-cart" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen 
+          name="orders" 
+          options={{
+            title: "Orders",
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name="list-alt" color={color} />
+            ),
+          }} 
+        />
+      </Tabs>
+    </SafeAreaView>
+  );
+};
+
+export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});

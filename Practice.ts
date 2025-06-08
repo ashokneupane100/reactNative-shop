5595a2992ce93de8f0b1418c427055fde833e0da
Ashok Neupane <ashokabrother@gmail.com>
2025-06-08 20:10:53 +0545

    Authentication page done #2.10

--- Practice.ts
+++ Practice.ts
@@ -1,127 +1,210 @@
 import {
-  ActivityIndicator,
-  FlatList,
-  ListRenderItem,
-  Pressable,
-  StyleSheet,
-  Text,
   View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
-import { Link, Stack } from 'expo-router';
-import { format } from 'date-fns';
+import { useForm, Controller } from 'react-hook-form';
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Redirect, Stack } from 'expo-router';
+import { supabase } from '../lib/supabase';
+import { Toast } from 'react-native-toast-notifications';
+import { useAuth } from '../providers/auth-provider';
 
-import { Tables } from '../../../types/database.types';
-import { getMyOrders } from '../../../api/api';
+const authSchema = zod.object({
+  email: zod.string().email({ message: 'Invalid email address' }),
+  password: zod
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+});
 
-const renderItem: ListRenderItem<Tables<'order'>> = ({ item }) => (
-  <Link href={`/orders/${item.slug}`} asChild>
-    <Pressable style={styles.orderContainer}>
-      <View style={styles.orderContent}>
-        <View style={styles.orderDetailsContainer}>
-          <Text style={styles.orderItem}>{item.slug}</Text>
-          <Text style={styles.orderDetails}>{item.description}</Text>
-          <Text style={styles.orderDate}>
-            {format(new Date(item.created_at), 'MMM dd, yyyy')}
-          </Text>
-        </View>
-        <View
-          style={[styles.statusBadge, styles[`statusBadge_${item.status}`]]}
-        >
-          <Text style={styles.statusText}>{item.status.toUpperCase()}</Text>
-        </View>
-      </View>
-    </Pressable>
-  </Link>
-);
+export default function Auth() {
+  const { session } = useAuth();
 
-const Orders = () => {
-  const { data: orders, error, isLoading } = getMyOrders();
+  if (session) return <Redirect href='/' />;
 
-  if (isLoading) return <ActivityIndicator />;
+  const { control, handleSubmit, formState } = useForm({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
 
-  if (error || !orders) return <Text>Error: {error?.message}</Text>;
+  const signIn = async (data: zod.infer<typeof authSchema>) => {
+    const { error } = await supabase.auth.signInWithPassword(data);
 
-  if (!orders.length)
-    return (
-      <Text
-        style={{
-          fontSize: 16,
-          color: '#555',
-          textAlign: 'center',
-          padding: 10,
-        }}
-      >
-        No orders created yet
-      </Text>
-    );
+    if (error) {
+      alert(error.message);
+    } else {
+      Toast.show('Signed in successfully', {
+        type: 'success',
+        placement: 'top',
+        duration: 1500,
+      });
+    }
+  };
+
+  const signUp = async (data: zod.infer<typeof authSchema>) => {
+    const { error } = await supabase.auth.signUp(data);
+
+    if (error) {
+      alert(error.message);
+    } else {
+      Toast.show('Signed up successfully', {
+        type: 'success',
+        placement: 'top',
+        duration: 1500,
+      });
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Stack.Screen options={{ title: 'Orders' }} />
-      <FlatList
-        data={orders}
-        keyExtractor={item => item.id.toString()}
-        renderItem={renderItem}
-      />
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      }}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.overlay} />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.subtitle}>Please Authenticate to continue</Text>
+
+        <Controller
+          control={control}
+          name='email'
+          render={({
+            field: { value, onChange, onBlur },
+            fieldState: { error },
+          }) => (
+            <>
+              <TextInput
+                placeholder='Email'
+                style={styles.input}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                placeholderTextColor='#aaa'
+                autoCapitalize='none'
+                editable={!formState.isSubmitting}
+              />
+              {error && <Text style={styles.error}>{error.message}</Text>}
+            </>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='password'
+          render={({
+            field: { value, onChange, onBlur },
+            fieldState: { error },
+          }) => (
+            <>
+              <TextInput
+                placeholder='Password'
+                style={styles.input}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                secureTextEntry
+                placeholderTextColor='#aaa'
+                autoCapitalize='none'
+                editable={!formState.isSubmitting}
+              />
+              {error && <Text style={styles.error}>{error.message}</Text>}
+            </>
+          )}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(signIn)}
+          disabled={formState.isSubmitting}
+        >
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.signUpButton]}
+          onPress={handleSubmit(signUp)}
+          disabled={formState.isSubmitting}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-};
+}
 
-export default Orders;
-
-const styles: { [key: string]: any } = StyleSheet.create({
-  container: {
+const styles = StyleSheet.create({
+  backgroundImage: {
     flex: 1,
-    padding: 16,
-  },
-  orderContainer: {
-    backgroundColor: '#f8f8f8',
-    padding: 16,
-    marginVertical: 8,
-    borderRadius: 8,
-  },
-  orderContent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    resizeMode: 'cover',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  orderDetailsContainer: {
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    width: '100%',
   },
-  orderItem: {
-    fontSize: 18,
+  title: {
+    fontSize: 36,
     fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 8,
   },
-  orderDetails: {
-    fontSize: 14,
-    color: '#555',
+  subtitle: {
+    fontSize: 18,
+    color: '#ddd',
+    marginBottom: 32,
   },
-  orderDate: {
-    fontSize: 12,
-    color: '#888',
-    marginTop: 4,
+  input: {
+    width: '90%',
+    padding: 12,
+    marginBottom: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 8,
+    fontSize: 16,
+    color: '#000',
   },
-  statusBadge: {
-    paddingVertical: 4,
-    paddingHorizontal: 8,
-    borderRadius: 4,
-    alignSelf: 'flex-start',
+  button: {
+    backgroundColor: '#6a1b9a',
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    width: '90%',
+    alignItems: 'center',
   },
-  statusText: {
-    fontSize: 12,
+  signUpButton: {
+    backgroundColor: 'transparent',
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+  signUpButtonText: {
+    color: '#fff',
+  },
+  buttonText: {
+    fontSize: 16,
     fontWeight: 'bold',
     color: '#fff',
   },
-  statusBadge_Pending: {
-    backgroundColor: '#ffcc00',
-  },
-  statusBadge_Completed: {
-    backgroundColor: '#4caf50',
-  },
-  statusBadge_Shipped: {
-    backgroundColor: '#2196f3',
-  },
-  statusBadge_InTransit: {
-    backgroundColor: '#ff9800',
+  error: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 16,
+    textAlign: 'left',
+    width: '90%',
   },
 });
